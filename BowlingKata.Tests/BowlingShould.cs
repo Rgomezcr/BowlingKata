a8365cacb560548df75533ae960bfdf02f6a4f19
Raúl Gómez <rgomez@voxelgroup.net>
2022-01-07 16:34:45 +0100

    test: NotScoreAnyPointsWhenNoPinsAreKnocked

--- BowlingKata.Tests/BowlingShould.cs
+++ BowlingKata.Tests/BowlingShould.cs
@@ -6,8 +6,13 @@ namespace BowlingKata.Tests
     public class BowlingShould
     {
         [Fact]
-        public void Test1()
+        public void NotScoreAnyPointsWhenNoPinsAreKnocked()
         {
+            Game game = new();
+
+            game.Roll(0);
+            
+            Assert.Equal(0,game.Score());
         }
     }
 }
---
+++ BowlingKata/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace BowlingKata
+{
+    public class Game
+    {
+        public int Score()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Roll(int pins)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

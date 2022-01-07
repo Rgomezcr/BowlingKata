9364effe8307cf0f321b42830f0f3a3121f356ca
Raúl Gómez <rgomez@voxelgroup.net>
2022-01-07 17:26:58 +0100

    feat: reset pins when changing frame

--- BowlingKata/Game.cs
+++ BowlingKata/Game.cs
@@ -1,22 +1,37 @@
 ﻿using System;
+using System.Linq;
 
 namespace BowlingKata
 {
     public class Game
     {
-        private int _pins;
+        private const int FramesPerGame = 10;
 
+        private int[] _pins = new int[FramesPerGame];
+        private int _currentFrame = 0;
+        private bool _isSecondRoll = false;
 
         public int Score()
         {
-            return _pins;
+            return _pins.Sum();
         }
 
         public void Roll(int pins)
         {
-            if (pins < 0 || pins + _pins > 10)
+            if (pins < 0 || pins + _pins[_currentFrame] > 10)
                 throw new ArgumentOutOfRangeException(nameof(pins));
-            _pins += pins;
+
+            _pins[_currentFrame] += pins;
+
+            if (_isSecondRoll)
+            {
+                _currentFrame++;
+                _isSecondRoll = false;
+            }
+            else
+            {
+                _isSecondRoll = true;
+            }
         }
     }
 }
